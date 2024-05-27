9816b547654b619b19f098c99b5d33b90da15a4a
NeTuNeO <149396653+NeTuNeO@users.noreply.github.com>
2024-05-27 15:04:46 +0300

    okuy

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://pindie2.nomoredomainswork.ru/api'
+export const BASE_URL = 'https://pindie2000.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
